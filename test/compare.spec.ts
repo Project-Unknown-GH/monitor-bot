668778dc54aa70602a773db2db0736eca4d9fa71
xWafl <73sampleperson@gmail.com>
2020-05-01 11:40:24 -0400

    :white_check_mark: Basic tests for comparison

---
+++ test/compare.spec.ts
@@ -0,0 +1,40 @@
+import { compareSingle } from "../compare";
+import { Item } from "../helpers";
+
+const expect = require("chai").expect;
+
+describe('Compare single', function () {
+    const templateItem: Omit<Item, "availability"> = {
+        "title": null,
+        "style": "Violet",
+        "link": "http://www.supremenewyork.com/shop/sweatshirts/al1qigtzr/iki14srab",
+        "description": "Cotton fleece with cutout logo appliqué at chest. ",
+        "addCartURL": null,
+        "price": 158,
+        "image": "https://assets.supremenewyork.com/188962/vi/hZiiM3_KhGM.jpg",
+        "sizesAvailable": null,
+        "images": [
+            "https://assets.supremenewyork.com/188962/zo/hZiiM3_KhGM.jpg",
+        ]
+    };
+    describe('Availability does not change', function () {
+        it('Should return sold out if it is still sold out', function () {
+            const item: Item = {...templateItem, ...{availability: "Sold Out"}};
+            expect(compareSingle(item, item)).to.deep.equal({changed: false, status: "Sold Out"});
+        });
+        it('Should return available if it is still available', function () {
+            const item: Item = {...templateItem, ...{availability: "Available"}};
+            expect(compareSingle(item, item)).to.deep.equal({changed: false, status: "Available"});
+        });
+    });
+    describe('Availability changes', function () {
+        const soldItem: Item = {...templateItem, ...{availability: "Sold Out"}};
+        const availItem: Item = {...templateItem, ...{availability: "Available"}};
+        it('Should return change from sold to available', function () {
+            expect(compareSingle(soldItem, availItem)).to.deep.equal({changed: true, status: "Available"});
+        });
+        it('Should return change from sold to available', function () {
+            expect(compareSingle(availItem, soldItem)).to.deep.equal({changed: true, status: "Sold Out"});
+        });
+    });
+});
