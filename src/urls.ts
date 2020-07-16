26752aba65460302278be6442be1e470160c51f1
xWafl <73sampleperson@gmail.com>
2020-07-16 13:26:15 -0400

    :sparkles: Url enum

---
+++ src/urls.ts
@@ -0,0 +1,5 @@
+export enum Webhooks {
+    TEST = "https://discordapp.com/api/webhooks/726133952469008397/aENg94lwsRmsCTVuKkOqZmcagZdschiCKdzzzoeBSDUMbKYoPVCvFiaV62ik3ST6-iSc",
+    UNFILTERED = "https://discordapp.com/api/webhooks/727200682439868449/J2TWA4AXHraVUo2eksMz7Snp47HSzQrw7mYv1SbsIjw9vwqosemw4uR5TgurFa1RjR5f",
+    FILTERED = "https://discordapp.com/api/webhooks/708285711006171156/wpGG8gARQpC0hY5ZG2g3zgzwNfeH0ryHGvnzxISRFIQ1dn7qwVB7YTO3XZhuAr1g-ChE"
+}
